791310ddd8482510c28955b88b6a35bec6045bb3
noeltimothy <noeltimothy@users.noreply.github.com>
2016-09-02 15:33:42 +0800

    Add files via upload

---
+++ examples/test-timsproxy.js
@@ -0,0 +1,32 @@
+
+var timsproxy = require ('./timsproxy.js')
+
+var p = new timsproxy (1000, 4000)
+
+p.on ('onConnect', function () {
+})
+
+p.on ('onResponse', function (res) {
+})
+
+p.on ('onRequest', function (req, res, cb) {
+
+	console.log ("Got req to =" + req.headers.host)
+
+	// test google safesearch enforcement
+	if (req.headers.host.match('google.com')) {
+		req.url += '&safe=active'
+	}
+
+	// test blocking of yahoo.com
+	if (req.headers.host.match('yahoo.com')) {
+		console.log ("setting state to block for yahoo.com")
+		cb.block(this, req, res)
+	} else {
+		cb.continue(this, req, res)
+	}
+})
+
+p.on ('error', function (err) {
+	console.log ("Got a error", err)
+})
